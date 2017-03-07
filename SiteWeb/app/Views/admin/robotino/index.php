d4dcb49e92f8b58d0766691f8ede493c59ec0695
Alan Dupin <alan-dupin@hotmail.fr>
2017-03-07 11:54:29 +0100

    Test déplacement robotino

--- SiteWeb/app/Views/admin/robotino/index.php
+++ SiteWeb/app/Views/admin/robotino/index.php
@@ -34,7 +34,7 @@
 <?php
 if(isset($_GET['action'])){
     echo '<meta http-equiv="Refresh" content="0; url=http://193.48.125.'.NUM_ROBOTINO.':50000/?action='.$_GET['action'].'">';
-    echo '<meta http-equiv="Refresh" content="5; url=<?= BASE_URL.\'/admin/robotino/\';?>">';
+    echo '<meta http-equiv="Refresh" content="2; url='.BASE_URL.'/admin/robotino/'.'">';
     /*sleep(1);
     header('Location: '.BASE_URL.'/admin/robotino/');
     exit();*/
