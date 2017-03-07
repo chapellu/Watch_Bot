a0b9d3ed64a7536f2abac699ec3f7d9581dd0334
Alan Dupin <alan-dupin@hotmail.fr>
2017-03-07 09:06:40 +0100

    Update URLs

--- SiteWeb/public/index.php
+++ SiteWeb/public/index.php
@@ -1,9 +1,11 @@
 <?php
 $debut = microtime(true);
-echo 'toto'; die();
+
 define('DS',DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__DIR__));
-define('BASE_URL',dirname(dirname($_SERVER['SCRIPT_NAME'])));
+//define('BASE_URL',dirname(dirname($_SERVER['SCRIPT_NAME'])));
+define('BASE_URL', '192.168.118.28/Watch_Bot/SiteWeb');
+define('NUM_ROBOTINO','38');
 require ROOT . '/app/App.php';
 
 App::load();
