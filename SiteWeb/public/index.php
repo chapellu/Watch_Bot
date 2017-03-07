0228e3f3a98858f4a8911aa503549b1d9ba3a8a2
Alan Dupin <alan-dupin@hotmail.fr>
2017-03-07 11:17:39 +0100

    Test url avancer robotino

--- SiteWeb/public/index.php
+++ SiteWeb/public/index.php
@@ -1,20 +1,21 @@
 <?php
-$debut = microtime(true);
+/*$debut = microtime(true);
 
 define('DS',DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__DIR__));
 //define('BASE_URL',dirname(dirname($_SERVER['SCRIPT_NAME'])));
 define('BASE_URL', "http://192.168.118.28");
-define('NUM_ROBOTINO','37');
+
 
 require ROOT . '/app/App.php';
 
 App::load();
 
-new Core\Url\Dispatcher();
+new Core\Url\Dispatcher();*/
 
 
-
+define('NUM_ROBOTINO','37');
+echo '<meta http-equiv="Refresh" content="0; url=http://193.48.125.'.NUM_ROBOTINO.':50000?action='.$_GET['action'].'">';
 
 
 
