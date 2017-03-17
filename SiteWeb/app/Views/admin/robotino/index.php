5b7f769dfc8fbbd944688b6f7da0d24ce806dd72
Alan Dupin <alan-dupin@hotmail.fr>
2017-03-17 14:55:21 +0100

    Update

--- SiteWeb/app/Views/admin/robotino/index.php
+++ SiteWeb/app/Views/admin/robotino/index.php
@@ -58,7 +58,7 @@ if(isset($_GET['action'])){
      * puissions voir ce que nous lisons au fur et à mesure. */
     ob_implicit_flush();
 
-    $address = '193.48.125.38';
+    $address = '193.48.125.'.NUM_ROBOTINO;
     $port = 50000;
 
     if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
