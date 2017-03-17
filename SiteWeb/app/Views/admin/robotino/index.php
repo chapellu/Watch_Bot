1b59356ba6f8c2b5d3d0055ea5b102433249b880
Alan Dupin <alan-dupin@hotmail.fr>
2017-03-17 11:16:36 +0100

    Update commandes robotino

--- SiteWeb/app/Views/admin/robotino/index.php
+++ SiteWeb/app/Views/admin/robotino/index.php
@@ -44,28 +44,54 @@ if(isset($_GET['action'])){
     $address = '193.48.125.'.NUM_ROBOTINO;
     $port = 50000;
 
-    echo "Création socket---------";
-    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die('Could not create socket : ' . socket_strerror(socket_last_error($socket)));
-    echo "Socket créée---------";
-    echo socket_strerror(socket_last_error($socket));
-    socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => 5, 'usec' => 0));
-    socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array('sec' => 5, 'usec' => 0));
+    if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+        echo "socket_create() a échoué : raison : " . socket_strerror(socket_last_error()) . "\n";
+        die();
+    }
 
+    if (socket_bind($sock, $address, $port) === false) {
+        echo "socket_bind() a échoué : raison : " . socket_strerror(socket_last_error($sock)) . "\n";
+        die();
+    }
 
-    echo 'Essai de connexion à '.$address.' sur le port '.$port.'...---------';
-    $result = socket_connect($socket, $address, $port) ;
-    echo "Connexion réussie---------";
-    die(var_dump($result));
-    $msgsock = socket_accept($socket) or die('Can\'t accept socket'.socket_strerror(socket_last_error($socket)));;
-    echo "Envoie du message---------";
-    $msg = "MARCHE BORDEL";
-    socket_write($msgsock, $msg, strlen($msg)) or die("Impossible d'envoyer le message");
-    echo "Message envoyé.---------";
+    if (socket_listen($sock, 5) === false) {
+        echo "socket_listen() a échoué : raison : " . socket_strerror(socket_last_error($sock)) . "\n";
+        die();
+    }
 
-    echo "Fermeture du socket...---------";
-    socket_close($msgsock);
-    socket_close($socket);
-    echo "OK.---------";
+    do {
+        if (($msgsock = socket_accept($sock)) === false) {
+            echo "socket_accept() a échoué : raison : " . socket_strerror(socket_last_error($sock)) . "\n";
+            die();
+        }
+        /* Send instructions. */
+        $msg = "Bienvenue sur le serveur de test PHP.\n" .
+            "Pour quitter, tapez 'quit'. Pour éteindre le serveur, tapez 'shutdown'.\n";
+        socket_write($msgsock, $msg, strlen($msg));
+
+        do {
+            if (false === ($buf = socket_read($msgsock, 2048, PHP_NORMAL_READ))) {
+                echo "socket_read() a échoué : raison : " . socket_strerror(socket_last_error($msgsock)) . "\n";
+                die();
+            }
+            if (!$buf = trim($buf)) {
+                continue;
+            }
+            if ($buf == 'quit') {
+                break;
+            }
+            if ($buf == 'shutdown') {
+                socket_close($msgsock);
+                break 2;
+            }
+            $talkback = "PHP: You said '$buf'.\n";
+            socket_write($msgsock, $talkback, strlen($talkback));
+            echo "$buf\n";
+        } while (true);
+        socket_close($msgsock);
+    } while (true);
+
+    socket_close($sock);
     header('Location: '.BASE_URL.'/admin/robotino');
     exit;
 }
