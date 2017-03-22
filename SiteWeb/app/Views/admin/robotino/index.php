1e8ca8f399951ddeff282748f30098f6a2d011f0
Alan Dupin <alan-dupin@hotmail.fr>
2017-03-22 14:38:26 +0100

    Update

--- SiteWeb/app/Views/admin/robotino/index.php
+++ SiteWeb/app/Views/admin/robotino/index.php
@@ -61,15 +61,9 @@ if(isset($_GET['action'])){
     $address = '193.48.125.'.NUM_ROBOTINO;
     $port = 50000;
 
-    if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+    if (($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
         die("socket_create() a échoué : raison : " . socket_strerror(socket_last_error()) . "\n");
     }
-    //Autorise l'utilisation d'adresse locales
-    if (socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1) === false) {
-        die('Impossible de définir l\'option du socket : '. socket_strerror(socket_last_error()) . PHP_EOL);
-    }
-
-
 
     echo "Essai de connexion à '$address' sur le port '$port'...";
     $result = socket_connect($socket, $address, $port);
