6b9ac7cd7c53846e8e237acc1a413f61f1188f79
Alan Dupin <alan-dupin@hotmail.fr>
2017-03-31 10:34:10 +0200

    Update D6T

--- scriptsD6T/readWithExactTemp.py
+++ scriptsD6T/readWithExactTemp.py
@@ -2,22 +2,11 @@ import omron, time
 
 if __name__ == '__main__':
     seuil = 28
+
     omron = omron.OmronD6T()
     omron.init()
 
     while True:
-        omron.read('auto',seuil)
+        omron.read('auto', seuil)
 
         time.sleep(1)
-
-        # for j in range(0,4):
-        # 	for i in range(0,4):
-        # 		if(omron.temperature[i+offset]>seuil):
-        # 			values.append("x ")
-        # 		else:
-        # 			values.append("o ")
-        # 	print values
-        # 	values = []
-        # 	print ''
-        # 	offset+=4
-        # time.sleep(1)
