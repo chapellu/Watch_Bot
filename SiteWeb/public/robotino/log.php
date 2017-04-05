23f5b993f9b2d7a9eea20afad202641214eec97f
Alan Dupin <alan-dupin@hotmail.fr>
2017-04-05 15:20:20 +0200

    Update

--- SiteWeb/public/robotino/log.php
+++ SiteWeb/public/robotino/log.php
@@ -2,9 +2,12 @@
     $log = fopen('/var/www/html/Watch_Bot/scriptsD6T/log.txt', 'r');
     $content='';
     //$log = fopen('C:\wamp64\www\watchbot/scriptsD6T/log.txt', 'r');
-    while(!feof($log)){
-        $ligne=fgets($log);
-        echo '<p>'.$ligne.'</p>';
+    if($log){
+        while(!feof($log)){
+            $ligne=fgets($log);
+            echo '<p>'.$ligne.'</p>';
+        }
+        fclose($log);
     }
-    fclose($log);
+
 ?>
