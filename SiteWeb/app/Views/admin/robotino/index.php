13452d1cd9bf7e89458cb9da30a54ef8cb022dd7
Alan Dupin <alan-dupin@hotmail.fr>
2017-04-26 17:14:03 +0200

    Update

--- SiteWeb/app/Views/admin/robotino/index.php
+++ SiteWeb/app/Views/admin/robotino/index.php
@@ -52,7 +52,7 @@ if(isset($_GET['action'])){
         fclose($log);
     }
     else if($action==='camera'){
-        exec('sudo  -u www-data bash /etc/init.d/watchbot-camera start > /dev/null 2>/dev/null &', $msg);
+        exec('bash /etc/init.d/watchbot-camera start > /dev/null 2>/dev/null &', $msg);
         var_dump($msg);die();
         $camera = True;
     }
