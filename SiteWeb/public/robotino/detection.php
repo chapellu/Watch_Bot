b2386f9f06517855914e9e1c7e2eb07777b46193
Alan Dupin <alan-dupin@hotmail.fr>
2017-04-28 15:42:25 +0200

    Update

--- SiteWeb/public/robotino/detection.php
+++ SiteWeb/public/robotino/detection.php
@@ -3,6 +3,7 @@
 $detection = fopen('/var/www/html/Watch_Bot/scriptsD6T/flagscript.txt', 'r');
 $content='';
 if($detection){
+    var_dump($detection);
     $ligne = fgets($detection);
     $seuil = fgets($detection) ? fgets($detection) : '0';
     App::getInstance()->detection_en_cours = strstr($ligne,'script=False') ? True : False;
