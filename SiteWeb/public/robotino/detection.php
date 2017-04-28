7436cd78808f6e7b68dcdb79f5e4607a51226a41
Alan Dupin <alan-dupin@hotmail.fr>
2017-04-28 16:25:50 +0200

    Update

--- SiteWeb/public/robotino/detection.php
+++ SiteWeb/public/robotino/detection.php
@@ -6,37 +6,34 @@ $seuil = fgets($flagscript);
 $detection = strstr($ligne,'script=False') ? false : true;
 fclose($flagscript);
 
-echo '
+?>
 
-<form action="'.BASE_URL.'/admin/robotino/" method="post">
+<form action="<?=BASE_URL.'/admin/robotino/';?>" method="post">
     <div class="input-group">
         <span class="input-group-addon" id="basic-addon1">Seuil</span>
-        <input type="number" class="form-control" name="seuil" value="'.$seuil.'" placeholder="Veuillez saisir une température" aria-describedby="basic-addon1">
+        <?= 'ok';?>
+        <input type="number" class="form-control" name="seuil" value="<?= $seuil;?>" placeholder="Veuillez saisir une température" aria-describedby="basic-addon1">
+        <?= 'plus ok';?>
     </div>
     <table class="table-with-spaces">
         <tr>
             <td>
-                
-                <input class="btn btn-success';if($detection){echo 'disabled';}echo '" type="submit" value="Lancer la detection">
-                <!-- <?= $form->bouttonRobotino(\'start-detection\', \'success\', \'Lancer la detection\');?>-->
+                <input class="btn btn-success <?php if($detection){echo 'disabled';}?>" type="submit" value="Lancer la detection">
+                <!-- <?= $form->bouttonRobotino('start-detection', 'success', 'Lancer la detection');?>-->
             </td>
-            <td>';
+            <td>
 
-
+                <?php
                 if($detection){
-                    echo $form->bouttonRobotino('stop-detection','danger', 'Arreter la detection');
+                    echo $form->bouttonRobotino('stop-detection','danger ', 'Arreter la detection');
                 } else {
                     echo $form->bouttonRobotino('stop-detection','danger disabled', 'Arreter la detection');
                 }
 
-echo '
+                ?>
 
             </td>
         </tr>
     </table>
 </form>
-';
-?>
-
-
 
