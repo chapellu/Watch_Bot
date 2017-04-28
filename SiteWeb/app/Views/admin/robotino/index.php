c5c4a499c41329d56b510b1787bc9fea944e9e3a
Alan Dupin <alan-dupin@hotmail.fr>
2017-04-28 12:58:38 +0200

    Update

--- SiteWeb/app/Views/admin/robotino/index.php
+++ SiteWeb/app/Views/admin/robotino/index.php
@@ -50,7 +50,7 @@ if(isset($_GET['action'])){
             $msg = '{"AuteurPrecedent":{"nom":"Site web","IP":"193.48.125.196"},"Destinataire":{"nom":"Raspberry","IP":"193.48.125.196"},"Date":{"date_string":'.date("Y-m-d-H-i-s").',"date":"'.date("M d, Y H:i:s a").'"},"type":Ordre,"message":"startSurveillance"}
         ';
 
-            App::sendSocket('193.48.125.196',50003,$msg);
+            //App::sendSocket('193.48.125.196',50003,$msg);
             die();
         }
     }
