fba90f9bbe0e6346162e7584868f0279b05734b9
Ludovic CH <ludovic.chapelet@etu.univ-savoie.fr>
2017-05-03 22:36:14 +0200

    Update Mail.java

--- Communication/APPRS2018/src/Communication/Mail.java
+++ Communication/APPRS2018/src/Communication/Mail.java
@@ -1,69 +1,125 @@
-public static void sendMessage(String subject, String text, String destinataire, String PATH_TO_FILE) {
-	// 1 -> Création de la session
-    Properties properties = new Properties();
-    properties.setProperty("mail.transport.protocol", "smtp");
-    properties.setProperty("mail.smtp.host", SMTP_HOST1);
-    properties.setProperty("mail.smtp.user", LOGIN_SMTP1);
-    properties.setProperty("mail.from", IMAP_ACCOUNT1);
-    Session session = Session.getInstance(properties);
-	// 2 -> Création du message
-	MimeMessage message = new MimeMessage(session);
-    try {
-        message.setText(text);
-        message.setSubject(subject);
-        message.addRecipients(Message.RecipientType.TO, destinataire);
-    } catch (MessagingException e) {
-        e.printStackTrace();
-    }
-	
-	File file = new File(PATH_TO_FILE);
-	FileDataSource datasource1 = new FileDataSource(file);
-	DataHandler handler1 = new DataHandler(datasource1);
-	MimeBodyPart autruche = new MimeBodyPart();
-	try {
-		autruche.setDataHandler(handler1);
-		autruche.setFileName(datasource1.getName());
-	} catch (MessagingException e) {
-		e.printStackTrace();
-	}
-	
-	MimeBodyPart content = new MimeBodyPart();
-	try {
-		content.setContent("Texte du message", "text/plain");
-	} catch (MessagingException e) {
-		e.printStackTrace();
-	}
-	
-	MimeMultipart mimeMultipart = new MimeMultipart();
-	try {
-		mimeMultipart.addBodyPart(content);
-		mimeMultipart.addBodyPart(autruche);
-		mimeMultipart.addBodyPart(musique);
-	} catch (MessagingException e) {
-		e.printStackTrace();
-	}
-
-    
-	try {
-		message.setContent(mimeMultipart);
-	} catch (MessagingException e) {
-		e.printStackTrace();
-	}
-	// 3 -> Envoi du message
-    Transport transport;
-    try {
-        transport = session.getTransport("smtp");
-        transport.connect(LOGIN_SMTP1, PASSWORD_SMTP1);
-        transport.sendMessage(message, new Address[] { new InternetAddress(destinataire),
-                                                        new InternetAddress(copyDest) });
-    } catch (MessagingException e) {
-        e.printStackTrace();
-    } finally {
-        try {
-            if (transport != null) {
-                transport.close();
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Properties;
+ 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Authenticator;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+ 
+/**
+ * @author Crunchify.com
+ * 
+ */
+ 
+public class Mail {
+ 
+    static Properties mailServerProperties;
+    static Session getMailSession;
+    static MimeMessage msg;
+ 
+    public static void main(String args[]) throws AddressException, MessagingException {
+ 
+        System.out.println("\n1st ===> setup Mail Server Properties..");
+ 
+        final String sourceEmail = "civ.odul74@gmail.com"; // requires valid Gmail id
+        final String password = "code-civ2.0"; // correct password for Gmail id
+        final String toEmail = "civ.odul74@gmail.com"; // any destination email id
+ 
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+ 
+        System.out
+                .println("\n2nd ===> create Authenticator object to pass in Session.getInstance argument..");
+ 
+        Authenticator authentication = new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(sourceEmail, password);
             }
+        };
+        Session session = Session.getInstance(props, authentication);
+        generateAndSendEmail(
+                session,
+                toEmail,
+                "Crunchify's JavaMail API example with Image Attachment",
+                "Greetings, <br><br>Test email by Crunchify.com JavaMail API example. Please find here attached Image."
+                        + "<br><br> Regards, <br>Crunchify Admin");
+ 
+    }
+ 
+    public static void generateAndSendEmail(Session session, String toEmail, String subject,
+            String body) {
+        try {
+            System.out.println("\n3rd ===> generateAndSendEmail() starts..");
+ 
+            MimeMessage crunchifyMessage = new MimeMessage(session);
+            crunchifyMessage.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            crunchifyMessage.addHeader("format", "flowed");
+            crunchifyMessage.addHeader("Content-Transfer-Encoding", "8bit");
+ 
+            crunchifyMessage.setFrom(new InternetAddress("noreply@crunchify.com",
+                    "NoReply-Crunchify"));
+            crunchifyMessage.setReplyTo(InternetAddress.parse("noreply@crunchify.com", false));
+            crunchifyMessage.setSubject(subject, "UTF-8");
+            crunchifyMessage.setSentDate(new Date());
+            crunchifyMessage.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(toEmail, false));
+ 
+            // Create the message body part
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(body, "text/html");
+ 
+            // Create a multipart message for attachment
+            Multipart multipart = new MimeMultipart();
+ 
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+ 
+            messageBodyPart = new MimeBodyPart();
+ 
+            // Valid file location
+            String filename = "D:/Ludovic/Images/IMG_19042017_191510_0.png";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            // Trick is to add the content-id header here
+            messageBodyPart.setHeader("Content-ID", "image_id");
+            multipart.addBodyPart(messageBodyPart);
+ 
+            System.out.println("\n4th ===> third part for displaying image in the email body..");
+            messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent("<br><h3>Find below attached image</h3>"
+                    + "<img src='cid:image_id'>", "text/html");
+            multipart.addBodyPart(messageBodyPart);
+            crunchifyMessage.setContent(multipart);
+ 
+            System.out.println("\n5th ===> Finally Send message..");
+ 
+            // Finally Send message
+            Transport.send(crunchifyMessage);
+ 
+            System.out
+                    .println("\n6th ===> Email Sent Successfully With Image Attachment. Check your email now..");
+            System.out.println("\n7th ===> generateAndSendEmail() ends..");
+ 
         } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
